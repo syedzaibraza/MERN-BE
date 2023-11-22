3620b9ca485ffee4914e6f24434d42e2de76a21e
PANDA <syedzaibraza@gmail.com>
2023-11-22 20:45:13 +0000

    Bcrypt Password ,create User bcrypt

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,4 +1,7 @@
-const Home = async (req: any, res: any) => {
+import { Request, Response } from "express";
+import User from "../models/user-model";
+import bcrypt from "bcrypt";
+const Home = async (req: Request, res: Response) => {
   try {
     res.status(200).json({ message: "hello world" });
   } catch (error) {
@@ -8,8 +11,20 @@ const Home = async (req: any, res: any) => {
 
 const Register = async (req: any, res: any) => {
   try {
-    console.log(req.body);
-    res.status(200).json({ message: req.body });
+    const { name, email, password, contact } = req.body;
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      res.status(400).json({ message: "User already exists" });
+    } else {
+      await User.create({
+        name,
+        email,
+        password,
+        contact,
+      });
+
+      res.status(200).json({ message: "User created successfully" });
+    }
   } catch (error) {
     console.log(error);
   }
